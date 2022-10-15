ebc64ce983d745b2bc85e4151fddaee795edf4fd
Jhony M. Navarro M <dacksokel@gmail.com>
2022-10-15 14:35:47 -0400

    update modal edit para devices

--- src/components/DispositivosConfig.vue
+++ src/components/DispositivosConfig.vue
@@ -7,6 +7,7 @@
           <th>Vendor</th>
           <th>Creation</th>
           <th>Status</th>
+          <th>Editar</th>
         </tr>
       </thead>
       <tbody>
@@ -21,34 +22,18 @@
           <td>{{ dispositivo.creation }}</td>
           <td>{{ dispositivo.status }}</td>
           <td>
-              <label for="my-modal-3" class="btn modal-button">open modal</label>
+              <label :for="`${dispositivo.id}my-modal-3`" class="btn modal-button">Editar</label>
           </td>
         </tr>
       </tbody>
     </table>
   </div>
-  <!-- The button to open modal -->
-
-  <!-- Put this part before </body> tag -->
-  <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-  <div class="modal">
-    <div class="modal-box relative">
-      <label
-        for="my-modal-3"
-        class="btn btn-sm btn-circle absolute right-2 top-2"
-        >✕</label
-      >
-      <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-      <p class="py-4">
-        You've been selected for a chance to get one year of subscription to use
-        Wikipedia for free!
-      </p>
-    </div>
-  </div>
-</template>
+  <EditDevicesModal v-for="device in gateway.devices" :id="device.id" :device="device"/>
+  </template>
 
 <script setup>
-import { useGateway } from "@/composables/useGateway";
+import { useGateway } from "@/composables/useGateway"
+import EditDevicesModal from './Modals/EditDevicesModal.vue'
 const { gateway } = useGateway();
 
 const showModal = () => {};
---
+++ src/components/Modals/EditDevicesModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- The button to open modal -->
+
+  <!-- Put this part before </body> tag -->
+  <input type="checkbox" :id="`${id}my-modal-3`" class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box relative">
+      <label
+        :for="`${id}my-modal-3`"
+        class="btn btn-sm btn-circle absolute right-2 top-2"
+        >✕</label
+      >
+      <h3 class="text-lg font-bold">
+        Congratulations random Internet user! este es el id: {{ id }}
+      </h3>
+      <p class="py-4">
+       {{device}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(["id", "device"]);
+</script>
+
+<style scoped></style>
