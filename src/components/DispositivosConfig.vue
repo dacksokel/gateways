4f83148b6355d8b9a6d6a00e3c9290cf1d3c11a3
Jhony M. Navarro M <dacksokel@gmail.com>
2022-10-15 12:30:39 -0400

    configurado correctamente tailwing y daysiui

--- src/components/DispositivosConfig.vue
+++ src/components/DispositivosConfig.vue
@@ -1,40 +1,31 @@
 <template>
-    <table class="table-auto">
-        <thead>
-            <tr>
-                <th>
-                    ID
-                </th>
-                <th>
-                    Vendor
-                </th>
-                <th>
-                    Creation
-                </th>
-                <th>
-                    Status
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="dispositivo in gateway.devices" :key="dispositivo.id">
-                
-                <td>{{dispositivo.id}}</td>
-                <td>{{dispositivo.vendor}}</td>
-                <td>{{dispositivo.creation}}</td>
-                <td>{{dispositivo.status}}</td>
-            </tr>
-        </tbody>
+  <div class="overflow-x-auto">
+    <table class="table w-full">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Vendor</th>
+          <th>Creation</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="hover" v-for="dispositivo in gateway.devices" :key="dispositivo.id">
+          <td>{{ dispositivo.id }}</td>
+          <td>{{ dispositivo.vendor }}</td>
+          <td>{{ dispositivo.creation }}</td>
+          <td>{{ dispositivo.status }}</td>
+        </tr>
+      </tbody>
     </table>
+  </div>
 </template>
 
 <script setup>
-import { useGateway } from '@/composables/useGateway'
-const { gateway } = useGateway()
+import { useGateway } from "@/composables/useGateway";
+const { gateway } = useGateway();
 
 console.log("dispositvios");
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
