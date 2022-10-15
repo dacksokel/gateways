70864ee148cee2997be3cea56ea4c74e3921d9c7
Jhony M. Navarro M <dacksokel@gmail.com>
2022-10-15 15:52:56 -0400

    pequenas correcciones

--- src/components/Modals/AddDevicesModal.vue
+++ src/components/Modals/AddDevicesModal.vue
@@ -1,29 +1,44 @@
 <template>
-    <!-- The button to open modal -->
-    <label for="my-modal-3" class="btn modal-button">Agregar Dispositivo</label>
+  <!-- The button to open modal -->
+  <label for="my-modal-3" class="btn modal-button">Agregar Dispositivo</label>
 
-    <!-- Put this part before </body> tag -->
-    <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-    <div class="modal">
-        <div class="modal-box relative">
-            <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-            <h3 class="text-lg font-bold">Agregar Dispositivos</h3>
-            <p class="py-4">
-                <input type="text" v-model="vendor" placeholder="Ingrese el nombre del Proveedor/ Vendor">
-            </p>
-            <p class="py-4">
-                <label class="label cursor-pointer">
-                    <span class="label-text">Offline/Online</span>
-                    <input type="checkbox" class="toggle toggle-primary" :checked="status" v-model="status"/>
-                </label>
-            </p>
-            <button class="btn btn-outline btn-primary" @click="addD">Guardar</button>
-        </div>
+  <!-- Put this part before </body> tag -->
+  <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box relative">
+      <label
+        for="my-modal-3"
+        class="btn btn-sm btn-circle absolute right-2 top-2"
+        >✕</label
+      >
+      <h3 class="text-lg font-bold">Agregar Dispositivos</h3>
+      <p class="py-4">
+        <label class="label">Vendor:</label>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+          v-model="vendor"
+        />
+      </p>
+      <p class="py-4">
+        <label class="label cursor-pointer">
+          <span class="label-text">Status Offline/Online</span>
+          <input
+            type="checkbox"
+            class="toggle toggle-primary"
+            :checked="status"
+            v-model="status"
+          />
+        </label>
+      </p>
+      <button class="btn btn-outline btn-primary" @click="addD">Guardar</button>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 import { useGateway } from "@/composables/useGateway";
 
 const { gateway, addDevice } = useGateway();
@@ -31,16 +46,14 @@ const vendor = ref(`dispositivo`);
 const status = ref(true);
 
 const addD = () => {
-    let dispositivo = {
-        id: gateway.value.devices.length + 1,
-        vendor: vendor.value,
-        creation: new Date("Jul 12 2022"),
-        status: status.value
-    }
-    addDevice(dispositivo)
-}
+  let dispositivo = {
+    id: gateway.value.devices.length + 1,
+    vendor: vendor.value,
+    creation: new Date("Jul 12 2022"),
+    status: status.value,
+  };
+  addDevice(dispositivo);
+};
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
--- src/components/Modals/EditDevicesModal.vue
+++ src/components/Modals/EditDevicesModal.vue
@@ -11,17 +11,39 @@
         >✕</label
       >
       <h3 class="text-lg font-bold">
-        Congratulations random Internet user! este es el id: {{ id }}
+        Editar Dispositivo {{ id }} {{ device.vendor }}
       </h3>
       <p class="py-4">
-       {{device}}
+        <label class="label">Vendor:</label>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+          v-model="device.vendor"
+        />
       </p>
+      <p class="py-4">
+        <label class="label cursor-pointer">
+          <span class="label-text">Status Offline/Online</span>
+
+          <input
+            type="checkbox"
+            class="toggle toggle-primary"
+            :checked="device.status"
+            v-model="device.status"
+          />
+        </label>
+      </p>
+      <div class="modal-action">
+      <label :for="`${id}my-modal-3`" class="btn">Guardar</label>
+    </div>
     </div>
   </div>
 </template>
 
 <script setup>
 defineProps(["id", "device"]);
+
 </script>
 
 <style scoped></style>
