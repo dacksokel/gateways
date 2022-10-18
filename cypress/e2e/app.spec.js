a8d46847af928d1b13d638a83db9c199459d9c06
Jhony M. Navarro M <dacksokel@gmail.com>
2022-10-18 18:51:19 -0400

    update listo agregada todas las pruebas e2e

--- cypress/e2e/app.spec.js
+++ cypress/e2e/app.spec.js
@@ -1,16 +1,50 @@
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+
+const numero = getRandomInt(200);
+const email = `${numero}@${numero}.com`;
+const password = "123456789";
+
+describe("registra un nuevo usuario", () => {
+  it("validando ruta de registro", () => {
+    cy.visit("/registro");
+    cy.get("h2").contains("Register");
+  });
+  it("Ingresando datos para registrar correo/password", () => {
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="password"]').type(password);
+  });
+
+  it("Validando datos  ingresados para registrar correo/password", () => {
+    cy.get('input[type="email"]').should("have.value", email);
+    cy.get('input[type="password"]').should("have.value", password);
+  });
+
+  it("Creando Registro de usuario", () => {
+    cy.get("button").contains("create").click();
+    cy.get("span").contains("Registro Existoso 🎉!!");
+   
+  });
+  it("logout", () => {
+     cy.wait(5000);
+    cy.contains("Sing out").click();
+  });
+});
 
 describe("Login, Modifcar gateway, agrega y borra un dispositivo", () => {
   it("visits the app root url", () => {
     cy.visit("/");
 
-    cy.contains("h2", "Login")
+    cy.contains("h2", "Login");
   });
   it("introducciendo datos a los inputs de email/password", () => {
     cy.get('input[type="email"]').type("1@1.co");
     cy.get('input[type="password"]').type("123456789");
   });
   it("validando que los inputs de email/password tengan los datos correctos", () => {
-    cy.get('input[type="email"]').should("have.value","1@1.co");
+    cy.get('input[type="email"]').should("have.value", "1@1.co");
     cy.get('input[type="password"]').should("have.value", "123456789");
   });
   it("limpia los inputs de email/password", () => {
@@ -18,10 +52,10 @@ describe("Login, Modifcar gateway, agrega y borra un dispositivo", () => {
     cy.get('input[type="email"]').clear();
   });
   it("hacer login con email/password bien", () => {
-    cy.get('input[type="email"]').type("2@2.com");
-    cy.get('input[type="password"]').type("123456");
-    cy.get('input[type="email"]').should("have.value", "2@2.com");
-    cy.get('input[type="password"]').should("have.value", "123456");
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="password"]').type(password);
+    cy.get('input[type="email"]').should("have.value", email);
+    cy.get('input[type="password"]').should("have.value", password);
     cy.get("button").first().click();
     cy.contains("span", "Login correcto :D");
   });
@@ -51,26 +85,45 @@ describe("Login, Modifcar gateway, agrega y borra un dispositivo", () => {
       .should("have.value", ip);
   });
 
-  it("guardando los datos", () => { 
+  it("guardando los datos", () => {
     cy.get("#guardar").contains("Guardar").click();
     cy.contains("span", "Se a guardados los cambios exitosamente");
     cy.visit("/dashboard/config/dispositivos");
-  })
-  it("agregando dispositivo", () => { 
+  });
+  it("agregando dispositivo", () => {
     cy.get("label").first().contains("Agregar Dispositivo").click();
     cy.get("#vendorinput").type("Router Prueba E2E");
     cy.get("button").contains("Guardar").click();
-  })
-  it("borrar dispositivo agregado", () => {     
+  });
+  it("borrar dispositivo agregado", () => {
     cy.get("td button").contains("Delete").click();
-  })
+  });
   it("logout", () => {
+     cy.wait(5000);
     cy.contains("Sing out").click();
   });
-
-
 });
 
-describe("registra un nuevo usuario", () => { 
-  
-})
+describe("Restaurando contrasena", () => {
+  it("validando ruta de registro", () => {
+    cy.visit("/resetPassword");
+    cy.get("h2").contains("Recovery Password");
+  });
+  it("Ingresando datos para registrar correo/password", () => {
+    cy.get('input[type="email"]').type(email);
+    cy.get("form button").contains("Validar").click();
+    cy.get("span").contains("El Correo Ingresado es valido");
+  });
+
+  it("ingresando nueva password", () => {
+    cy.get('input[type="password"]').type("987654");
+    cy.get('input[type="password"]').should("have.value", "987654");
+  });
+
+  it("Validando Cambio de password", () => {
+    cy.get(".modal-action button").contains("Guardar").click();
+    cy.get("span").contains(
+      `Password Cambiado Exitosamente!! al correo ${email}`
+    );
+  });
+});
